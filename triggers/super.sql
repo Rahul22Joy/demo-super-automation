972930ec4252027050a2d2aae48153d992680dd5
rahul22joy <rahul22joy@gmail.com>
2018-01-09 15:47:20 +1100

    changes

--- triggers/super.sql
+++ triggers/super.sql
@@ -3,7 +3,7 @@ group default_triggers
 priority 1
 comment 'State changess rahul processing for ALERTS.STATUS'
 before update on alerts.status
-for each row
+for each row (
 begin
         set new.StateChange = getdate();
 end;
