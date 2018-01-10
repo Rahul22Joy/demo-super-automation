8d06a75edc846edd737a542368315391625df766
rahul22joy <rahul22joy@gmail.com>
2018-01-10 18:09:52 +1100

    rahul changes

--- triggers/super.sql
+++ triggers/super.sql
@@ -5,7 +5,7 @@ comment 'State changess rahul processing for ALERTS.STATUS'
 before update on alerts.status
 for each row 
 begin
-        set new.StateChange = getdate();)
+        set new.StateChange = getdate();
 --comment rahul joy 123
 end;
 go
